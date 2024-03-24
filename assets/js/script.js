9aadcfed188753e8f9f7e6e1bde2ae9611c93781
Cristian Díaz <cristian.diaz@gmail.com>
2024-03-24 15:59:33 -0300

    Desafío 4 completo

---
+++ assets/js/script.js
@@ -0,0 +1,184 @@
+const propiedades_venta = [ 
+    {
+        nombre: 'Apartamento de lujo en zona exclusiva',
+        src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+        descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+        ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+        habitaciones: 4,
+        baños: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false 
+    },
+    {
+        nombre: 'Apartamento acogedor en la montaña',
+        src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+        descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+        ubicacion: '89 Mountain Road, Summit Peaks, CA 23456',
+        habitaciones: 2,
+        baños: 1,
+        costo: 1200,
+        smoke: true,
+        pets: true 
+    },
+    {
+        nombre: 'Penthouse de lujo con terraza panorámica',
+        src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+        descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+        ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+        habitaciones: 3,
+        baños: 3,
+        costo: 4500,
+        smoke: false,
+        pets: true 
+    },
+    {
+        nombre: 'Departamento 160 m2 en Providencia',
+        src: "https://www.almagro.cl/wp-content/uploads/2024/01/lyon-bilbao-listado-nueva-1.jpg",
+        descripcion: 'Hemos diseñado cada rincón para que puedas hacer todo eso que siempre quisiste en un solo lugar',
+        ubicacion: 'Ricardo Lyon 1887, Providencia',
+        habitaciones: 4,
+        baños: 4,
+        costo: 3000,
+        smoke: true,
+        pets: true 
+    }   
+]
+
+const propiedades_alquiler = [ 
+    {
+        nombre: 'Apartamento en el centro de la ciudad',
+        src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2000,
+        smoke: false,
+        pets: true 
+    }, 
+    {
+        nombre: 'Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+        habitaciones: 3,
+        baños: 3,
+        costo: 2500,
+        smoke: true,
+        pets: true 
+    }, 
+    {
+        nombre: 'Condominio moderno en zona residencial',
+        src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2200,
+        smoke: false,
+        pets: false 
+    }, 
+    {
+        nombre: 'Departamento funcional en Providencia',
+        src: 'https://www.almagro.cl/wp-content/uploads/2020/11/huascar-listado-nuevo.jpg',
+        descripcion: 'Departamentos en Providencia diseñados para generar mayor luminosidad y amplitud en los espacios.',
+        ubicacion: 'Huáscar 1300, Providencia',
+        habitaciones: 2,
+        baños: 1,
+        costo: 1500,
+        smoke: true,
+        pets: true 
+    }
+]
+
+function generaLista(tipo, listado, maximo = (listado.length)) {
+    let html=""
+    let htmlSmoke = ""
+    let htmlPets = ""
+    let contador = 1
+    for (let propiedad of listado) {
+        // Detener el loop
+        if (contador > maximo) {
+            break
+        }
+        // Revisar si es para fumador o no
+        if (propiedad.smoke == true) {
+            htmlSmoke = `<p class="text-success">
+                <i class="fas fa-smoking"></i> Permitido fumar
+            </p>
+            `
+        } else {
+            htmlSmoke = `<p class="text-danger">
+                <i class="fas fa-smoking-ban"></i> No se permite fumar
+            </p>
+            `
+        }
+        // Revisar si es para mascotas o no
+        if (propiedad.pets == true) {
+            htmlPets = `<p class="text-success">
+                <i class="fas fa-paw"></i> Mascotas permitidas
+            </p>
+            `
+        } else {
+            htmlPets = `<p class="text-danger">
+                <i class="fa-solid fa-ban"></i> No se permiten mascotas
+            </p>
+            `
+        }
+        html += `<div class="col-md-4 mb-4">
+                <div class="card">
+                    <img
+                        src="${propiedad.src}"
+                        class="card-img-top"
+                        alt="Imagen del departamento"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            ${propiedad.nombre}
+                        </h5>
+                        <p class="card-text">
+                            ${propiedad.descripcion}
+                        </p>
+                        <p>
+                            <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion} 
+                        </p>
+                        <p>
+                            <i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |
+                            <i class="fas fa-bath"></i> ${propiedad.baños} Baños
+                        </p>
+                        <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
+                        ${htmlSmoke}
+                        ${htmlPets}
+                    </div>
+                </div>
+            </div>
+        `
+        contador ++
+    }
+    return html
+}
+
+const ventaIndex = document.querySelector(".venta-index")
+if (ventaIndex != null) {
+    const listaVentaIndex = generaLista("venta", propiedades_venta, 3)
+    ventaIndex.innerHTML = listaVentaIndex
+}
+
+const alquilerIndex = document.querySelector(".alquiler-index")
+if (alquilerIndex != null) {
+    const listaAlquilerIndex = generaLista("alquiler", propiedades_alquiler, 3)
+    alquilerIndex.innerHTML = listaAlquilerIndex
+}
+
+const ventaTotal = document.querySelector(".venta-total")
+if (ventaTotal != null) {
+    const listaVenta = generaLista("venta", propiedades_venta)
+    ventaTotal.innerHTML = listaVenta
+}
+
+const alquilerTotal = document.querySelector(".alquiler-total")
+if (alquilerTotal != null) {
+    const listaAlquiler = generaLista("alquiler", propiedades_alquiler)
+    alquilerTotal.innerHTML = listaAlquiler
+}
